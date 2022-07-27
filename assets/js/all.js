a7b5d4df1a484d75ab2418caf4a00c7944b865f7
Olivia <olivia@MacBook-Air.local>
2022-07-28 00:13:46 +0800

    Update 2022-07-27T16:13:41.607Z

---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  $('.nav-ham').click(function () {
+    $(this).toggleClass('open');
+    $('.nav').toggleClass('show');
+  });
+});
+//# sourceMappingURL=all.js.map
